4135f095b7b4794fdb484d843f766b662851302a
sanush <maleekasanush@gmail.com>
2019-10-30 14:06:06 +0530

    biggest update

--- app/src/main/java/com/example/wakemeup/MapActivity.kt
+++ app/src/main/java/com/example/wakemeup/MapActivity.kt
@@ -6,12 +6,13 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
-import android.os.Build
-import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
+import android.os.*
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -27,6 +28,7 @@ import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_map.*
 import kotlinx.android.synthetic.main.card_view.*
 import org.jetbrains.anko.vibrator
@@ -42,18 +44,20 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerC
     private lateinit var mMap: GoogleMap
     private var click_status: Int = 0
     private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var lastLocation: Location
+    private var lastLocation: Location? = null
     private lateinit var locationCallback: LocationCallback
     private lateinit var locationRequest: LocationRequest
     private var locationUpdateState = false
     lateinit var destination: String
     lateinit var destinationLatLng: LatLng
     lateinit var currentLatLng: LatLng
-    var distance: Int = 0
+    var distance: Float = 2000.0F
     lateinit var mStarterMarker: Marker
     lateinit var mEndMarker: Marker
 
     lateinit var fileds: List<Place.Field>
+
+    var isOpen: Boolean = false
 
     lateinit var polylines: List<Polyline>
     private val COLORS = intArrayOf(R.color.polyLine)
@@ -62,6 +66,55 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerC
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        val context = this
+
+        val myLocation = findViewById<ImageView>(R.id.iconMyLocation)
+
+        myLocation.setOnClickListener {
+            setUpMap()
+        }
+
+        val item_view = AnimationUtils.loadAnimation(applicationContext, R.anim.open)
+        val item_hide = AnimationUtils.loadAnimation(applicationContext, R.anim.close)
+        val clock = AnimationUtils.loadAnimation(applicationContext, R.anim.clock_wise)
+        val re_clock = AnimationUtils.loadAnimation(applicationContext, R.anim.re_clock_wise)
+
+        options.setOnClickListener {
+            if (isOpen)
+            {
+                options.startAnimation(re_clock)
+                btnTheme.startAnimation(item_hide)
+                btnLogout.startAnimation(item_hide)
+                txtTheme.startAnimation(item_hide)
+                txtSignOut.startAnimation(item_hide)
+                btnTheme.isClickable = false
+                btnLogout.isClickable = false
+                txtSignOut.isClickable = false
+                txtTheme.isClickable = false
+                isOpen = false
+            }
+            else
+            {
+                options.startAnimation(clock)
+                btnTheme.startAnimation(item_view)
+                btnLogout.startAnimation(item_view)
+                txtTheme.startAnimation(item_view)
+                txtSignOut.startAnimation(item_view)
+                btnTheme.isClickable = true
+                btnLogout.isClickable = true
+                txtSignOut.isClickable = true
+                txtTheme.isClickable = true
+                isOpen = true
+            }
+        }
+
+        btnLogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
 
         polylines = ArrayList()
 
@@ -88,16 +141,27 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerC
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         locationCallback = object : LocationCallback() {
-            override fun onLocationResult(p0: LocationResult) {
-                super.onLocationResult(p0)
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations)
+                {
+                    lastLocation = location
+                    Toast.makeText(context, distance.toString(), Toast.LENGTH_SHORT).show()
+                    test()
+                }
 
-                lastLocation = p0.lastLocation
             }
         }
 
-
         createLocationRequest()
 
+        //Toast.makeText(this, distance.toString(), Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        vibrator.cancel()
     }
 
 
@@ -108,9 +172,40 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerC
 
     }
 
+    fun test()
+    {
+        if (distance <= 500) {
+            cardlayout.visibility = View.VISIBLE
+            if (Build.VERSION.SDK_INT >= 26) {
+                val vibrator: Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                vibrator.vibrate(
+
+                    VibrationEffect.createOneShot(
+                        10000,
+                        VibrationEffect.EFFECT_DOUBLE_CLICK
+                    )
+                )
+                okayBtn.setOnClickListener() {
+                    vibrator.cancel()
+                    cardlayout.visibility = View.GONE
+                }
+
+            } else {
+                vibrator.vibrate(10000)
+                okayBtn.setOnClickListener() {
+                    vibrator.cancel()
+                    cardlayout.visibility = View.GONE
+                }
+
+            }
+        }
+
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isZoomControlsEnabled = false
+        mMap.uiSettings.isMyLocationButtonEnabled = false
         mMap.setOnMarkerClickListener(this)
 
         var success = googleMap.setMapStyle(
@@ -163,10 +258,13 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerC
 
         locationRequest = LocationRequest()
 
-        locationRequest.interval = 10000
+        locationRequest.interval = 500
 
         locationRequest.fastestInterval = 5000
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        test()
+        Toast.makeText(this, distance.toString(), Toast.LENGTH_SHORT).show()
 
         val builder = LocationSettingsRequest.Builder()
             .addLocationRequest(locationRequest)
@@ -212,8 +310,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerC
         mFusedLocationProviderClient.requestLocationUpdates(
             locationRequest,
             locationCallback,
-            null /* Looper */
-        )
+            Looper.getMainLooper())
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -240,6 +337,19 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerC
                 val mCameraUpdate = CameraUpdateFactory.newLatLngZoom(destinationLatLng, 13F)
                 mMap.animateCamera(mCameraUpdate)
                 getPolyLines(destinationLatLng)
+
+                var lastLoc = Location("")
+                lastLoc.longitude = lastLocation!!.longitude
+                lastLoc.latitude = lastLocation!!.latitude
+
+
+                var destinationLoc = Location("")
+                destinationLoc.longitude = destinationLatLng.longitude
+                destinationLoc.latitude = destinationLatLng.latitude
+
+                distance = lastLoc.distanceTo(destinationLoc)
+                test()
+                Toast.makeText(this, distance.toString(), Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -249,7 +359,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerC
             .travelMode(AbstractRouting.TravelMode.TRANSIT)
             .withListener(this)
             .alternativeRoutes(true)
-            .waypoints(LatLng(lastLocation.latitude, lastLocation.longitude), destinationLatLng)
+            .waypoints(LatLng(lastLocation!!.latitude, lastLocation!!.longitude), destinationLatLng)
             .key("AIzaSyAG36QakYK2Q7Ma6bQlal4we7Vv6fKuks8")
             .build()
         routing.execute()
@@ -304,41 +414,6 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerC
             polyOptions.addAll(route.get(i).points)
             val polyline = mMap.addPolyline(polyOptions)
             (polylines as ArrayList<Polyline>).add(polyline)
-
-            distance = route.get(i).distanceValue
-            //Toast.makeText(applicationContext, "Route " + (i + 1) + ": distance - " + route.get(i).distanceValue + ": duration - " + route.get(i).durationValue, Toast.LENGTH_SHORT).show()
-            if (distance <= 100) {
-                cardlayout.visibility = View.VISIBLE
-                if (Build.VERSION.SDK_INT >= 26) {
-                    val vibrator: Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                        vibrator.vibrate(
-
-                            VibrationEffect.createOneShot(
-                                100,
-                                VibrationEffect.EFFECT_DOUBLE_CLICK
-                            )
-                        )
-//                    if(click_status !=0){
-//                        vibrator.cancel()
-//                    }
-                    okayBtn.setOnClickListener(){
-                        vibrator.cancel()
-                        cardlayout.visibility = View.GONE
-                    }
-
-                } else
-                {
-                        vibrator.vibrate(10000)
-//                    if(click_status !=0){
-//                        vibrator.cancel()
-//                    }
-                    okayBtn.setOnClickListener(){
-                        vibrator.cancel()
-                        cardlayout.visibility = View.GONE
-                    }
-
-                }
-            }
         }
     }
 
--- app/src/main/java/com/example/wakemeup/SplashActivity.kt
+++ app/src/main/java/com/example/wakemeup/SplashActivity.kt
@@ -1,4 +1,35 @@
 package com.example.wakemeup
 
-class SplashActivity {
+import android.content.Intent
+import android.graphics.PorterDuff
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    var progressStatus = 0
+    private val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val progressBar = findViewById<ProgressBar>(R.id.determinateBar)
+
+        Thread(Runnable {
+            while (progressStatus < 100) {
+                progressStatus++
+                android.os.SystemClock.sleep(20)
+                handler.post { progressBar.setProgress(progressStatus) }
+            }
+            if (progressStatus === 100) {
+                val intent = Intent(this@SplashActivity, MapActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }).start()
+
+    }
 }
